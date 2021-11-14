d2a0b545dc4cc31c9bfc872311ad42e390b7632a
Ryan Huynh <ryanhuynh@mwc-073149.dhcp.missouri.edu>
2021-11-14 15:50:18 -0600

    added text boxes to specify which cards to swap

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+
+
+
+//Move card animation
+function swapCards() {
+    let leftText = document.getElementById('leftInput').value;
+    let rightText = document.getElementById('rightInput').value;
+
+    let id = null;
+    const card1 = document.getElementById(leftText);
+    const card2 = document.getElementById(rightText);
+    
+    let cardSize = document.querySelector("img.card").style.width;
+    console.log("cardSize = ",cardSize);
+
+    let pos = 0;
+
+    clearInterval(id);
+    /*
+    id = setInterval(moveApart,2);
+
+    function moveApart () {
+        if(pos == 100)
+            clearInterval(id);
+        else {
+            pos++;
+            card1.style.top = pos + 'px';
+            card2.style.top = -pos + 'px';
+        }
+    }
+    */
+
+    clearInterval(id);
+    id = setInterval(moveCardsOver,1);
+    pos = 0;
+    function moveCardsOver() {
+        if(pos == 100 + 4) //Length in pixels to move over (card width + gap)
+            clearInterval(id);
+        else {
+            pos++;
+            card1.style.left = pos + 'px';
+            card2.style.left = -pos + 'px';
+        }
+    }
+
+}
+
+/*
 let card1 = document.querySelector('#AC');
 let rect = card1.getBoundingClientRect();
 
@@ -30,45 +78,4 @@ function linedraw(ax,ay,bx,by)
     var length=Math.sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
     document.body.innerHTML += "<div id='line' style='height:" + length + "px;width:1px;background-color:black;position:absolute;top:" + (ay) + "px;left:" + (ax) + "px;transform:rotate(" + calc + "deg);-ms-transform:rotate(" + calc + "deg);transform-origin:0% 0%;-moz-transform:rotate(" + calc + "deg);-moz-transform-origin:0% 0%;-webkit-transform:rotate(" + calc  + "deg);-webkit-transform-origin:0% 0%;-o-transform:rotate(" + calc + "deg);-o-transform-origin:0% 0%;'></div>"
 }
-
-//linedraw(card1X,card1Y,1000,1000);
-//console.log("after linedraw");
-
-//Move card animation
-function moveCardLeft() {
-    let id = null;
-    const card1 = document.getElementById('AC');
-    const card2 = document.getElementById('QH');
-    
-    let cardSize = document.querySelector("img.card").style.width;
-    console.log("cardSize = ",cardSize);
-
-    let pos = 0;
-
-    clearInterval(id);
-    id = setInterval(moveApart,3);
-
-    function moveApart () {
-        if(pos == 100)
-            clearInterval(id);
-        else {
-            pos++;
-            card1.style.top = pos + 'px';
-            card2.style.top = -pos + 'px';
-        }
-    }
-
-    clearInterval(id);
-    id = setInterval(moveCardsOver,3);
-    pos = 0;
-    function moveCardsOver() {
-        if(pos == 100 + 4) //Length in pixels to move over (card width + gap)
-            clearInterval(id);
-        else {
-            pos++;
-            card1.style.left = pos + 'px';
-            card2.style.left = -pos + 'px';
-        }
-    }
-
-}
+*/
