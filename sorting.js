a44f700696d8d0e2a5fb0f6df05f37bc441da086
Ryan Huynh <ryanhuynh@Ryans-Air-2.home>
2022-01-10 20:16:18 -0600

    added new bubble sort button

---
+++ sorting.js
@@ -0,0 +1,28 @@
+//Has the sort functions
+
+//Compares two cards
+//If c1 > c2, returns 1
+//If the cards are the same, returns 0
+//If c1 < c2, returns -1
+//If error, returns false
+function compare(c1,c2) {
+    //Check if the cards are in the current deck
+    if(!cardSet.has(c1) || !cardSet.has(c2)) {
+        alert("Error: at least one of the cards is not a valid playing card");
+        return false;
+    }
+    
+    if(c1 == c2)
+        return 0;
+
+}
+
+//Sorts the cards and displays them
+function bubbleSort() {
+
+}
+
+//Called when the user clicks the bubble sort button
+function bubbleSortOnClick() {
+    bubbleSort(cardArray);
+}
