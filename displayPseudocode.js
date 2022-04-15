78d513e7bda4fbc37c090f48ea94f0b8591ab143
Ryan Huynh <ryanhuynh@mu-582547.dhcp.missouri.edu>
2022-04-15 11:13:58 -0500

    write functions to display pseudocode

--- displayPseudocode.js
+++ displayPseudocode.js
@@ -10,3 +10,32 @@ let bubbleSortText = `function bubblesort(array) {
        }
     }
  }`;
+
+/*
+Clears the pseudocode from the screen
+*/
+function clearPseudocode() {
+    let cardDiv = document.getElementById('pseudocode-container');
+    cardDiv.innerHTML = "";
+    codeOnScreen = false;
+}
+
+/*
+Displays pseudocode for the current sorting algorithm
+Parameters:
+ - pseudocodeText: the text of the pseudocode to be displayed
+*/
+function displayPseudocode(pseudocodeText) {
+    let cardDiv = document.getElementById('pseudocode-container');
+    cardDiv.innerHTML = "";
+
+    let preElement = document.createElement("pre");
+    preElement.innerText = pseudocodeText;
+    
+    if(codeOnScreen) {
+        clearPseudocode();
+    }
+
+    cardDiv.append(preElement);
+    codeOnScreen = true;
+}
