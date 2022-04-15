c4012b6fa8b3161df2fa0a3734d5540e360483b6
Ryan Huynh <ryanhuynh@mu-582547.dhcp.missouri.edu>
2022-04-15 11:05:54 -0500

    commit uncommited changes

--- displayPseudocode.js
+++ displayPseudocode.js
@@ -2,3 +2,11 @@
 
 let codeOnScreen = false; //Global boolean that marks whether pseudocode is currently on the screen
 
+let bubbleSortText = `function bubblesort(array) {
+    for(i = 0; i < array.length; i++) {
+        for(j = 0; j < array.length - i - 1; j++) {
+            if(array[j] > array[j + 1]) 
+               swap(array[j], array[j + 1]);
+       }
+    }
+ }`;
